bdb2a1e67ee1f931c3ff9e4c2ec63e945193e9e6
carsonhinsverk <carsonhinsverk@gmail.com>
2026-07-15 21:08:42 -0500

    Updated paths to fit linux and redid how data loaders are made and used in training.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from random import random
 from shutil import ignore_patterns
 
 from torch import optim
 
 from device import device
 from Models.baseline import STATT, WSTATT
-from data import get_data_loaders
+from data import get_random_sample, get_data_loader
 
 import torch
 
@@ -34,6 +35,7 @@ if __name__ == "__main__":
     )
 
     models = [statt, wstatt]
+    print("COMPLETED")
 
     for model in models:
         print("#######################################################################")
@@ -44,7 +46,6 @@ if __name__ == "__main__":
         optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
         train_loss = []
-        data_loaders = get_data_loaders(num_epochs, batch_size)
 
         for epoch in range(num_epochs):
             print(f'## EPOCH {epoch} ##')
@@ -53,8 +54,12 @@ if __name__ == "__main__":
 
             epoch_loss = 0
 
-            for (idx, data_loader) in enumerate(data_loaders):
-                loader_loss = 0
+            dataset = get_random_sample()
+
+            for grid_num, grid in enumerate(dataset):
+                data_loader = get_data_loader(grid, batch_size)
+
+                grid_loss = 0
 
                 for batch, [image_patch, label_patch] in enumerate(data_loader):
                     optimizer.zero_grad()
@@ -68,12 +73,12 @@ if __name__ == "__main__":
                     batch_loss.backward() 
                     optimizer.step()     
 
-                    loader_loss += batch_loss.item()
+                    grid_loss += batch_loss.item()
 
-                loader_loss = loader_loss / (batch + 1) 
-                print(f'\tLoader Number: {idx}\Loader:{data_loader} \Loader Loss:{loader_loss}')
-                epoch_loss += loader_loss
+                grid_loss = grid_loss / (batch + 1) 
+                print(f'\tGrid Number: {grid_num} Grid:{grid} Grid Loss:{grid_loss}')
+                epoch_loss += grid_loss
 
-            epoch_loss = epoch_loss / (idx + 1)
+            epoch_loss = epoch_loss / (grid_num + 1)
             print(f'\nTrain Loss:{epoch_loss}')
             train_loss.append(epoch_loss) 
