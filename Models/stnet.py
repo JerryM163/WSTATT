227bfc8a2accaf6d209cce3ad8a6ccb957dc1094
Carson Hinsverk <carsonhinsverk@gmail.com>
2026-07-28 15:48:43 -0500

    and more

--- Models/stnet.py
+++ Models/stnet.py
@@ -34,6 +34,8 @@ class TemporalAttentionPooling(torch.nn.Module):
     def forward(self, x, context):
         # Apply previous context to scores when relevant
         if context is not None:
+            print(x.shape)
+            print(context.shape)
             x += self.context_proj(context).unsqueeze(1)
 
         # Compute scores
@@ -224,8 +226,6 @@ class STNET(torch.nn.Module):
 
         # Temporal pooling on the 1st transformer's output
         pooled1, alpha1, context1 = self.temp_pool1(trans1, None) # Output: (batches*32*32,64)
-        print(pooled1.shape)
-        print(context1.shape)
 
         # Reshape transformer's output for maxpooling
         trans1 = trans1.reshape(batches,height,width,timestamps,64)
