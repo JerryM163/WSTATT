46f5e2510607ae4001fca4148d73d1d6160bede4
Carson Hinsverk <carsonhinsverk@gmail.com>
2026-07-29 00:22:33 -0500

    Incorrect output sizes

--- Models/stnet.py
+++ Models/stnet.py
@@ -239,7 +239,7 @@ class STNET(torch.nn.Module):
         # Temporal pooling on the 1st transformer's output
         pooled1, alpha1, context1 = self.temp_pool1(trans1, None) # Output: (batches*32*32,64)
 
-        context1 = self.downsample_context(context1,batches,32,32,context_dim=64)
+        context1 = self.downsample_context(context1,batches,32,16,context_dim=64)
 
         # Reshape transformer's output for maxpooling
         trans1 = trans1.reshape(batches,height,width,timestamps,64)
@@ -262,7 +262,7 @@ class STNET(torch.nn.Module):
         # Temporal pooling on the 2nd transformer's output
         pooled2, alpha2, context2 = self.temp_pool2(trans2, context1) # Output: (batches*16*16,128)
 
-        context2 = self.downsample_context(context2,batches,16,16,context_dim=64)
+        context2 = self.downsample_context(context2,batches,16,8,context_dim=64)
 
         trans2 = trans2.reshape(batches,16,16,timestamps,128)
         trans2 = trans2.permute(0,3,4,1,2)
