cd498ffb7d3acef88ff592456c6703227c52664d
Carson Hinsverk <carsonhinsverk@gmail.com>
2026-07-29 10:02:42 -0500

    Testing

--- Models/stnet.py
+++ Models/stnet.py
@@ -234,7 +234,8 @@ class STNET(torch.nn.Module):
         # Temporal pooling on the 1st transformer's output
         pooled1, alpha1, context1 = self.temp_pool1(trans1, None) # Output: (batches*32*32,64)
 
-        sampled_context1 = self.downsample_context(context1,batches,32,16,context_dim=64)
+        # Spatially downsample context to fit with next temporal pooling layer
+        sampled_context1 = self.downsample_context(context1,batches,32,16,context_dim=64) # Output: (batches*16*16,64)
 
         # Reshape transformer's output for maxpooling
         trans1 = trans1.reshape(batches,height,width,timestamps,64)
@@ -257,7 +258,8 @@ class STNET(torch.nn.Module):
         # Temporal pooling on the 2nd transformer's output
         pooled2, alpha2, context2 = self.temp_pool2(trans2, sampled_context1) # Output: (batches*16*16,128)
 
-        sampled_context2 = self.downsample_context(context2,batches,16,8,context_dim=64)
+        # Spatially downsample context to fit with next temporal pooling layer
+        sampled_context2 = self.downsample_context(context2,batches,16,8,context_dim=64) # Output: (batches*8*8,64)
 
         trans2 = trans2.reshape(batches,16,16,timestamps,128)
         trans2 = trans2.permute(0,3,4,1,2)
