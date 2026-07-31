2eb149a0a58f30249dc35a8a64ece191d69e6f44
Carson Hinsverk <carsonhinsverk@gmail.com>
2026-07-30 23:12:38 -0500

    Dont know

--- test.py
+++ test.py
@@ -139,7 +139,7 @@ def test_model_preds(model, test_dataset, batch_size, timestamps, bands=[]):
 
         july = 6
 
-        heatmap = alpha1[july, :].reshape(32, 32)
+        heatmap = alpha1[july, :].reshape(24, 24)
 
         plt.imshow(heatmap.cpu(), cmap="viridis")
         plt.colorbar(label="Attention")
